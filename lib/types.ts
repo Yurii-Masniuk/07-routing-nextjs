42de97953ef39c6e337ec36a2183fa50e0eb38b9
Yurii-Masniuk <yeristar@gmail.com>
2025-11-01 12:49:43 +0200

    fixed all

--- lib/types.ts
+++ lib/types.ts
@@ -1,10 +1,34 @@
-export type NoteTag = "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
+// export type NoteTag = "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
+
+// export interface Note {
+//     id: string;
+//     title: string;
+//     content: string;
+//     tag: NoteTag;
+//     createdAt: string;
+//     updatedAt: string;
+// }
+
+// export interface NormalizedNotesResponse {
+//     data: Note[];
+//     meta: {
+//         totalItems: number;
+//         page: number;
+//         perPage: number;
+//         totalPages: number;
+//     };
+// }
+
+export type DefinedNoteTag = "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
+
+export type NoteTag = DefinedNoteTag | (string & {});
+export type NoteTagFromUrl = DefinedNoteTag | 'all' | string;
 
 export interface Note {
     id: string;
     title: string;
     content: string;
-    tag: NoteTag;
+    tag: DefinedNoteTag;
     createdAt: string;
     updatedAt: string;
 }
